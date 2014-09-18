4c474c90c62f38b218339504412531e55c394237
Amadeus Wieczorek <git@amadeusw.com>
2014-09-18 10:49:27 -0400

    Adds view for the login screen

---
+++ DemoApplication/DemoApplication/Converters/StepToVisibilityConverter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Windows;
+using System.Globalization;
+using System.Windows.Data;
+using AmadeusW.DemoApplication;
+
+namespace AmadeusW.DemoApplication.Converters
+{
+    [ValueConversion(typeof(ApplicationSteps), typeof(Visibility))]
+    class StepToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is ApplicationSteps))
+            {
+                return Visibility.Collapsed;
+            }
+
+            ApplicationSteps visibleStep = (ApplicationSteps)value;
+            String stepName = parameter.ToString();
+
+            if (visibleStep.ToString() == parameter.ToString())
+            {
+                return Visibility.Visible;
+            }
+            else
+            {
+                return Visibility.Collapsed;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is Visibility))
+            {
+                return 0;
+            }
+            Visibility stepVisibility = (Visibility)value;
+            String stepName = parameter.ToString();
+
+            if (stepVisibility == Visibility.Visible && stepName == "FirstStep")
+            {
+                return 1;
+            }
+            else if (stepVisibility == Visibility.Visible && stepName == "SecondStep")
+            {
+                return 2;
+            }
+            else if (stepVisibility == Visibility.Visible && stepName == "ThirdStep")
+            {
+                return 3;
+            }
+            else if (stepVisibility == Visibility.Visible && stepName == "FourthStep")
+            {
+                return 4;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
